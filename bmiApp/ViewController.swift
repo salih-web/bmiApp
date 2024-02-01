7670523379a0452dd75a370c156f31ba2141d7b5
salih-web <bulut._bulut@icloud.com>
2024-02-02 01:20:54 +0300

    Initial Commit

---
+++ bmiApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  bmiApp
+//
+//  Created by Muhammed Salih Bulut on 2.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var KiloLabel: UILabel!
+    @IBOutlet weak var uzunLabel: UILabel!
+    
+    @IBOutlet weak var uzunSlider: UISlider!
+    @IBOutlet weak var kiloSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func uzunSliderDegis(_ sender: UISlider) {
+    }
+    
+    @IBAction func kiloSliderDegis(_ sender: UISlider) {
+    }
+    
+    @IBAction func hesapButton(_ sender: UIButton) {
+    }
+}
+
